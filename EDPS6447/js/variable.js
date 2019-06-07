244101c90c0c2396b38f61999796674ba0b21ba3
Katie Woods <46548045+woods3km@users.noreply.github.com>
2019-06-06 18:47:47 -0600

    Add files via upload

---
+++ EDPS6447/js/variable.js
@@ -0,0 +1,29 @@
+console.log("Wilderness First Responder Pre-Course Work");
+
+//UI Components (e.g., Icons, Buttons)
+var input_button_wma=
+    document.getElementById("input_button_wma");
+    console.log(input_button_wma)
+var input_button_gp=
+    document.getElementById("input_button_gp");
+    console.log(input_button_gp)
+var input_button_pas=
+    document.getElementById("input_button_pas");
+    console.log(input_button_pas)
+var input_button_cs=
+    document.getElementById("input_button_cs");
+    console.log(input_button_cs)
+var input_button_msk=
+    document.getElementById("input_button_msk");
+    console.log(input_button_msk)
+var input_button_enviro=
+    document.getElementById("input_button_enviro");
+    console.log(input_button_enviro)
+var input_button_cs=
+    document.getElementById("input_button_cs");
+    console.log(input_button_cs)
+var input_button_proto=
+    document.getElementById("input_button_proto");
+    console.log(input_button_proto)
+
+
